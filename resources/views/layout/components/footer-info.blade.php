461b96ec870173f8a5434a722189adf58cb4c242
NizamHakim <5025211209@mhs.its.ac.id>
2023-12-21 22:44:52 +0700

    add demo link

--- resources/views/layout/components/footer-info.blade.php
+++ resources/views/layout/components/footer-info.blade.php
@@ -7,8 +7,8 @@
                 </div>
                 <div class="socials">
                     <ul class="flexitem">
-                        <li><a href="https://github.com/NizamHakim/E-Commerce-FP-PWeb" target="_blank"><i class="ri-github-line"></i></a></li>
-                        <li><a href="#" target="_blank"><i class="ri-youtube-line"></i></a></li>
+                        <li><a title="GitHub Repository" href="https://github.com/NizamHakim/E-Commerce-FP-PWeb" target="_blank"><i class="ri-github-line"></i></a></li>
+                        <li><a title="Demo" href="https://youtu.be/W2mq0JD1yAU" target="_blank"><i class="ri-youtube-line"></i></a></li>
                     </ul>
                 </div>
             </div>
