7d2639063df602c4a53d47c702130d2619d0b26b
Stoam <harimustofa188@gmail.com>
2023-12-21 13:42:37 +0700

    fix ItemSeeder: change the price from USD to IDR

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -24,7 +24,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Luxurious Moisturizing Cream',
                 'item_category_id' => '1',
-                'price' => '39.99',
+                'price' => '600000',
                 'description' => 'Indulge in this luxurious moisturizing cream for soft and supple skin.',
                 'image' => 'images/beauty/beauty_1.jpg',
                 'stock' => '100',
@@ -33,7 +33,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Radiant Skin Serum',
                 'item_category_id' => '1',
-                'price' => '49.99',
+                'price' => '750000',
                 'description' => 'Achieve a radiant complexion with our advanced skin serum.',
                 'image' => 'images/beauty/beauty_2.jpg',
                 'stock' => '150',
@@ -42,7 +42,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Natural Rosewater Toner',
                 'item_category_id' => '1',
-                'price' => '19.99',
+                'price' => '300000',
                 'description' => 'Refresh and tone your skin with our natural rosewater toner.',
                 'image' => 'images/beauty/beauty_3.jpg',
                 'stock' => '120',
@@ -51,7 +51,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Anti-Aging Night Cream',
                 'item_category_id' => '1',
-                'price' => '59.99',
+                'price' => '900000',
                 'description' => 'Reduce fine lines and wrinkles with our nourishing anti-aging night cream.',
                 'image' => 'images/beauty/beauty_4.jpg',
                 'stock' => '80',
@@ -60,7 +60,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Glowing Skin Elixir',
                 'item_category_id' => '1',
-                'price' => '29.99',
+                'price' => '450000',
                 'description' => 'Experience a natural glow with our specially formulated skin elixir.',
                 'image' => 'images/beauty/beauty_5.jpg',
                 'stock' => '90',
@@ -69,7 +69,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Hydrating Facial Mask',
                 'item_category_id' => '1',
-                'price' => '24.99',
+                'price' => '375000',
                 'description' => 'Revitalize your skin with our hydrating facial mask for a spa-like experience.',
                 'image' => 'images/beauty/beauty_6.jpg',
                 'stock' => '110',
@@ -80,7 +80,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Samsung Galaxy S21',
                 'item_category_id' => '2',
-                'price' => '799.99',
+                'price' => '12000000',
                 'description' => 'Experience the latest features with our flagship Samsung Galaxy S21 smartphone.',
                 'image' => 'images/electronic/electronic_1.jpg',
                 'stock' => '50',
@@ -89,7 +89,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Ultra HD Smart TV',
                 'item_category_id' => '2',
-                'price' => '899.99',
+                'price' => '13500000',
                 'description' => 'Immerse yourself in stunning visuals with our ultra HD smart TV.',
                 'image' => 'images/electronic/electronic_2.jpg',
                 'stock' => '30',
@@ -98,7 +98,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Wireless Noise-Canceling Headphones',
                 'item_category_id' => '2',
-                'price' => '149.99',
+                'price' => '2250000',
                 'description' => 'Enjoy crystal-clear audio with our wireless noise-canceling headphones.',
                 'image' => 'images/electronic/electronic_3.jpg',
                 'stock' => '100',
@@ -107,7 +107,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Gaming Laptop Pro',
                 'item_category_id' => '2',
-                'price' => '1299.99',
+                'price' => '19500000',
                 'description' => 'Level up your gaming experience with our powerful gaming laptop pro.',
                 'image' => 'images/electronic/electronic_4.jpg',
                 'stock' => '20',
@@ -116,7 +116,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Smart Home Security Camera',
                 'item_category_id' => '2',
-                'price' => '79.99',
+                'price' => '1200000',
                 'description' => 'Monitor your home with our smart home security camera for peace of mind.',
                 'image' => 'images/electronic/electronic_5.jpg',
                 'stock' => '75',
@@ -125,7 +125,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Bluetooth Wireless Earbuds',
                 'item_category_id' => '2',
-                'price' => '39.99',
+                'price' => '600000',
                 'description' => 'Enjoy the freedom of wireless audio with our Bluetooth earbuds.',
                 'image' => 'images/electronic/electronic_6.jpg',
                 'stock' => '60',
@@ -136,7 +136,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Stylish Denim Jacket',
                 'item_category_id' => '3',
-                'price' => '79.99',
+                'price' => '1200000',
                 'description' => 'Upgrade your style with our trendy denim jacket.',
                 'image' => 'images/fashion/fashion_1.jpg',
                 'stock' => '30',
@@ -145,7 +145,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Elegant Cocktail Dress',
                 'item_category_id' => '3',
-                'price' => '129.99',
+                'price' => '1950000',
                 'description' => 'Make a statement with our elegant cocktail dress for special occasions.',
                 'image' => 'images/fashion/fashion_2.jpg',
                 'stock' => '20',
@@ -154,7 +154,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Classic Leather Handbag',
                 'item_category_id' => '3',
-                'price' => '49.99',
+                'price' => '750000',
                 'description' => 'Complete your look with our classic leather handbag.',
                 'image' => 'images/fashion/fashion_3.jpg',
                 'stock' => '50',
@@ -163,7 +163,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Formal Men\'s Suit',
                 'item_category_id' => '3',
-                'price' => '199.99',
+                'price' => '3000000',
                 'description' => 'Look sharp and sophisticated in our formal men\'s suit.',
                 'image' => 'images/fashion/fashion_4.jpg',
                 'stock' => '25',
@@ -172,7 +172,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Comfortable Sneakers',
                 'item_category_id' => '3',
-                'price' => '59.99',
+                'price' => '900000',
                 'description' => 'Stay comfortable and stylish with our trendy sneakers.',
                 'image' => 'images/fashion/fashion_5.jpg',
                 'stock' => '40',
@@ -181,7 +181,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Chic Sunglasses',
                 'item_category_id' => '3',
-                'price' => '29.99',
+                'price' => '450000',
                 'description' => 'Shield your eyes with our chic sunglasses for a fashionable look.',
                 'image' => 'images/fashion/fashion_6.jpg',
                 'stock' => '35',
@@ -192,7 +192,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Stainless Steel Cookware Set',
                 'item_category_id' => '4',
-                'price' => '149.99',
+                'price' => '2250000',
                 'description' => 'Upgrade your kitchen with our durable stainless steel cookware set.',
                 'image' => 'images/home-kitchen/home_kitchen_1.jpg',
                 'stock' => '40',
@@ -201,7 +201,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Cozy Throw Blanket',
                 'item_category_id' => '4',
-                'price' => '29.99',
+                'price' => '450000',
                 'description' => 'Stay warm and stylish with our cozy throw blanket for chilly evenings.',
                 'image' => 'images/home-kitchen/home_kitchen_2.jpg',
                 'stock' => '60',
@@ -210,7 +210,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Smart Coffee Maker',
                 'item_category_id' => '4',
-                'price' => '79.99',
+                'price' => '1200000',
                 'description' => 'Brew your favorite coffee with our smart coffee maker with programmable features.',
                 'image' => 'images/home-kitchen/home_kitchen_3.jpg',
                 'stock' => '30',
@@ -219,7 +219,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Elegant Dining Set',
                 'item_category_id' => '4',
-                'price' => '249.99',
+                'price' => '3750000',
                 'description' => 'Dine in style with our elegant dining set for a sophisticated dining experience.',
                 'image' => 'images/home-kitchen/home_kitchen_4.jpg',
                 'stock' => '20',
@@ -228,7 +228,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Soft Microfiber Bed Sheets',
                 'item_category_id' => '4',
-                'price' => '39.99',
+                'price' => '600000',
                 'description' => 'Experience luxurious comfort with our soft microfiber bed sheets.',
                 'image' => 'images/home-kitchen/home_kitchen_5.jpg',
                 'stock' => '50',
@@ -237,7 +237,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Robotic Vacuum Cleaner',
                 'item_category_id' => '4',
-                'price' => '199.99',
+                'price' => '3000000',
                 'description' => 'Keep your home clean effortlessly with our advanced robotic vacuum cleaner.',
                 'image' => 'images/home-kitchen/home_kitchen_6.jpg',
                 'stock' => '25',
@@ -248,7 +248,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Interactive Cat Toy',
                 'item_category_id' => '5',
-                'price' => '19.99',
+                'price' => '300000',
                 'description' => 'Entertain your cat with our interactive toy for hours of fun.',
                 'image' => 'images/pet-supplies/pet_supplies_1.jpg',
                 'stock' => '50',
@@ -257,7 +257,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Premium Dog Food',
                 'item_category_id' => '5',
-                'price' => '29.99',
+                'price' => '450000',
                 'description' => 'Provide your dog with the finest nutrition using our premium dog food.',
                 'image' => 'images/pet-supplies/pet_supplies_2.jpg',
                 'stock' => '40',
@@ -266,7 +266,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Cozy Pet Bed',
                 'item_category_id' => '5',
-                'price' => '49.99',
+                'price' => '750000',
                 'description' => 'Give your pet a comfortable place to rest with our cozy pet bed.',
                 'image' => 'images/pet-supplies/pet_supplies_3.jpg',
                 'stock' => '30',
@@ -275,7 +275,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Automatic Pet Feeder',
                 'item_category_id' => '5',
-                'price' => '79.99',
+                'price' => '1200000',
                 'description' => 'Schedule regular meals for your pet with our automatic pet feeder.',
                 'image' => 'images/pet-supplies/pet_supplies_4.jpg',
                 'stock' => '25',
@@ -284,7 +284,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Cat Grooming Kit',
                 'item_category_id' => '5',
-                'price' => '34.99',
+                'price' => '525000',
                 'description' => 'Keep your cat looking sharp with our comprehensive cat grooming kit.',
                 'image' => 'images/pet-supplies/pet_supplies_5.jpg',
                 'stock' => '35',
@@ -293,7 +293,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Durable Dog Leash',
                 'item_category_id' => '5',
-                'price' => '14.99',
+                'price' => '225000',
                 'description' => 'Take your dog for a walk with our durable and stylish dog leash.',
                 'image' => 'images/pet-supplies/pet_supplies_6.jpg',
                 'stock' => '60',
@@ -304,7 +304,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Tennis Racket Set',
                 'item_category_id' => '6',
-                'price' => '89.99',
+                'price' => '1350000',
                 'description' => 'Ace your game with our high-quality tennis racket set.',
                 'image' => 'images/sports/sports_1.jpg',
                 'stock' => '30',
@@ -313,7 +313,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Adjustable Dumbbell Set',
                 'item_category_id' => '6',
-                'price' => '199.99',
+                'price' => '3000000',
                 'description' => 'Build strength with our adjustable dumbbell set for home workouts.',
                 'image' => 'images/sports/sports_2.jpg',
                 'stock' => '20',
@@ -322,7 +322,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Soccer Ball',
                 'item_category_id' => '6',
-                'price' => '24.99',
+                'price' => '375000',
                 'description' => 'Kick off your soccer game with our durable and reliable soccer ball.',
                 'image' => 'images/sports/sports_3.jpg',
                 'stock' => '40',
@@ -331,7 +331,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Running Shoes',
                 'item_category_id' => '6',
-                'price' => '59.99',
+                'price' => '900000',
                 'description' => 'Hit the track with our comfortable and supportive running shoes.',
                 'image' => 'images/sports/sports_4.jpg',
                 'stock' => '25',
@@ -340,7 +340,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Fitness Resistance Bands Set',
                 'item_category_id' => '6',
-                'price' => '29.99',
+                'price' => '450000',
                 'description' => 'Enhance your workout with our versatile fitness resistance bands set.',
                 'image' => 'images/sports/sports_5.jpg',
                 'stock' => '50',
@@ -349,7 +349,7 @@ class ItemSeeder extends Seeder
             [
                 'name' => 'Portable Basketball Hoop',
                 'item_category_id' => '6',
-                'price' => '129.99',
+                'price' => '1950000',
                 'description' => 'Practice your slam dunk with our portable basketball hoop for indoor or outdoor use.',
                 'image' => 'images/sports/sports_6.jpg',
                 'stock' => '35',
